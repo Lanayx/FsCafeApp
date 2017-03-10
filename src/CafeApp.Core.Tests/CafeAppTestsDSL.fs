8a8c6582940ddd31e332f80fa895828ff75c3b42
Vladimir Schchur <vladimir.shchur@idt.net>
2017-03-10 23:28:20 +0300

    Open Tab first unit test

---
+++ src/CafeApp.Core.Tests/CafeAppTestsDSL.fs
@@ -0,0 +1,15 @@
+﻿module CafeAppTestsDSL
+open FsUnit
+open NUnit.Framework
+open CommandHandlers
+open States
+
+let Given (state : State) = state
+let When command state = (command, state)
+let ThenStateShouldBe expectedState (command, state) =
+  let actualState = evolve state command
+  actualState |> fst |> should equal expectedState
+  actualState |> snd
+
+let WithEvents expectedEvents actualEvents =
+  actualEvents |> should equal expectedEvents
---
+++ src/CafeApp.Core/States.fs
@@ -0,0 +1,16 @@
+﻿module States
+open Domain
+open System
+open Events
+
+type State =
+  | ClosedTab of Guid option
+  | OpenedTab of Tab
+  | PlacedOrder of Order
+  | OrderInProgress of InProgressOrder
+  | ServedOrder of Order
+
+let apply state event =
+  match state,event with
+  | ClosedTab _, TabOpened tab -> OpenedTab tab
+  | _ -> state
