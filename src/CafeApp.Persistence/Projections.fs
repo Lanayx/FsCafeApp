b3c5b0df86ccb038c2fa0b9e26fa91b058e6035f
Vladimir Shchur <vladimir.shchur@idt.net>
2017-03-19 11:48:50 +0300

    Finished projections and queries

--- src/CafeApp.Persistence/Projections.fs
+++ src/CafeApp.Persistence/Projections.fs
@@ -6,21 +6,33 @@ open System
 type TableActions = {
   OpenTab : Tab -> Async<unit>
   ReceivedOrder : Guid -> Async<unit>
+  CloseTab: Tab -> Async<unit>
+}
+
+type CashierActions = {
+  AddTabAmount: Guid -> decimal -> Async<unit>
+  Remove: Guid -> Async<unit>
 }
 
 type ChefActions = {
   AddFoodsToPrepare : Guid -> Food list -> Async<unit>
+  RemoveFood: Guid -> Food -> Async<unit>
+  Remove: Guid -> Async<unit>
 }
 
 type WaiterActions = {
   AddDrinksToServe : Guid -> Drink list -> Async<unit>
   MarkDrinkServed : Guid -> Drink -> Async<unit>
+  AddFoodToServe: Guid -> Food -> Async<unit>
+  MarkFoodServed: Guid -> Food -> Async<unit>
+  Remove: Guid -> Async<unit>
 }
 
 type ProjectionActions = {
   Table : TableActions
   Waiter : WaiterActions
   Chef : ChefActions
+  Cashier: CashierActions
 }
 
 let projectReadModel actions = function
@@ -36,4 +48,24 @@ let projectReadModel actions = function
 | DrinkServed (item, tabId) ->
   [actions.Waiter.MarkDrinkServed tabId item]
   |> Async.Parallel
-| _ -> failwith "TODO"
+| FoodPrepared (food, tabId) ->
+  [
+    actions.Chef.RemoveFood tabId food
+    actions.Waiter.AddFoodToServe tabId food
+  ]|> Async.Parallel
+| FoodServed (food, tabId) ->
+  [actions.Waiter.MarkFoodServed tabId food]
+  |> Async.Parallel
+| OrderServed (order, payment) ->
+  let tabId = order.Tab.Id
+  [
+    actions.Waiter.Remove tabId
+    actions.Chef.Remove tabId
+    actions.Cashier.AddTabAmount tabId payment.Amount
+  ]|> Async.Parallel
+| TabClosed payment ->
+  let tabId = payment.Tab.Id
+  [
+    actions.Cashier.Remove tabId
+    actions.Table.CloseTab payment.Tab
+  ] |> Async.Parallel
---
+++ src/CafeApp.Persistence/Queries.fs
@@ -0,0 +1,20 @@
+﻿module Queries
+open ReadModel
+open Domain
+open States
+open System
+
+type TableQueries = {
+  GetTables: unit -> Async<Table list>
+}
+
+type ToDoQueries = {
+  GetChefToDos: unit -> Async<ChefToDo list>
+  GetWaiterToDos: unit -> Async<WaiterToDo list>
+  GetCashierToDos: unit -> Async<Payment list>
+}
+
+type Queries = {
+  Table: TableQueries
+  ToDo: ToDoQueries
+}
