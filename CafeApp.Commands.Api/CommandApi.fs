9083c89834d1cfafc2541718be1d126a5d1c5087
Vladimir Shchur <vladimir.shchur@idt.net>
2017-03-25 21:42:21 +0300

    Added web project

---
+++ CafeApp.Commands.Api/CommandApi.fs
@@ -0,0 +1,15 @@
+﻿module CommandApi
+
+open System.Text
+open CommandHandlers
+open Queries
+open OpenTab
+open Chessie.ErrorHandling
+
+let handleCommandRequest queries eventStore
+  = function
+  | OpenTabRequest tab ->
+    queries.Table.GetTableByTableNumber
+    |> openTabCommander
+    |> handleCommand eventStore tab
+  | _ -> err "Invalid command" |> fail |> async.Return
